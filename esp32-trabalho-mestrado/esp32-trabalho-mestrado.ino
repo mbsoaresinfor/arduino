afab5186b3a64e6190a17cbf9a3933a17e419d9f
Marcelo Barbosa Soares <marcelo.soares@cwi.com.br>
2021-05-10 20:26:41 -0300

    conexao com gw

--- esp32-trabalho-mestrado/esp32-trabalho-mestrado.ino
+++ esp32-trabalho-mestrado/esp32-trabalho-mestrado.ino
@@ -2,6 +2,7 @@
 #include <WiFi.h>
 #include <OneWire.h> // sensor temperatura
 #include <DallasTemperature.h> // sensor temperatura
+#include <HTTPClient.h>
 
 const int sensorTemperaturaPin = 23;
 
@@ -14,19 +15,11 @@ DeviceAddress endereco_temp; // Cria um endereco temporario da leitura do sensor
 const char* ssid     = "MBS_EXT";
 const char* password = "10607620";
 
-// ThingSpeak information
-char thingSpeakAddress[] = "api.thingspeak.com";
-unsigned long channelID = 1350904;
-char* readAPIKey = "C669YRGVI4DF2L03";
-char* writeAPIKey = "QD4A1OL80XP6Y9K0";
-const unsigned long postingInterval = 120L * 1000L;
-unsigned int dataFieldOne = 1;                            // Field to write temperature data
-unsigned int dataFieldTwo = 2;                       // Field to write temperature data
-
 unsigned long lastConnectionTime = 0;
 long lastUpdateTime = 0; 
 WiFiClient client;
 
+String endpoint_update = "http://192.168.0.12:8080/update";
 
 void setup() {
 
@@ -36,11 +29,39 @@ void setup() {
 }
 
 void loop() {
-    delay(15000);     
-
+    
   float temperature = leituraSensorTemperatura();
 
-  writeTSData( channelID , dataFieldOne , temperature );  
+  if(WiFi.status()== WL_CONNECTED){
+     Serial.println("WiFi Connected");
+      HTTPClient http;
+
+      String serverPath = endpoint_update + "?field=1&value=" + temperature;
+
+      Serial.println("Connecting " + serverPath);
+      
+      http.begin(serverPath.c_str());
+      
+      int httpResponseCode = http.GET();
+      
+      if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+        Serial.println(payload);
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      // Free resources
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    delay(5000);     
+  
 }
 
 void connectWiFi(){
@@ -53,39 +74,9 @@ void connectWiFi(){
     }
     
     Serial.println( "Connected" );
-    ThingSpeak.begin( client );
+ 
 }
 
-
-float readTSData( long TSChannel,unsigned int TSField ){
-    
-  float data =  ThingSpeak.readFloatField( TSChannel, TSField, readAPIKey );
-  Serial.println( " Data read from ThingSpeak: " + String( data, 9 ) );
-  return data;
-
-}
-
-// Use this function if you want to write a single field.
-int writeTSData( long TSChannel, unsigned int TSField, float data ){
-  int  writeSuccess = ThingSpeak.writeField( TSChannel, TSField, data, writeAPIKey ); // Write the data to the channel
-  if ( writeSuccess ){
-    
-    Serial.println( String(data) + " written to Thingspeak. Status: " + writeSuccess );
-    }
-    
-    return writeSuccess;
-}
-
-// Use this function if you want to write multiple fields simultaneously.
-int write2TSData( long TSChannel, unsigned int TSField1, float field1Data, unsigned int TSField2, long field2Data, unsigned int TSField3, long field3Data ){
-
-  ThingSpeak.setField( TSField1, field1Data );
-  ThingSpeak.setField( TSField2, field2Data );
-  ThingSpeak.setField( TSField3, field3Data );
-   
-  int writeSuccess = ThingSpeak.writeFields( TSChannel, writeAPIKey );
-  return writeSuccess;
-}
 
 float leituraSensorTemperatura(){
   float temperatura = 0;
